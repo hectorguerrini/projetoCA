cd96d90caa8d6202f3e912a3441dc3697bf43018
HectorGuerrini <hectorumaga@hotmail.com>
2018-06-08 01:00:19 -0300

    ajustes

--- src/providers/pca/pca.ts
+++ src/providers/pca/pca.ts
@@ -11,7 +11,7 @@ import { URLSearchParams } from '@angular/http';
 @Injectable()
 export class PcaProvider {
 
-  private urlApi = 'http://caeem.br/vendas/'
+  private urlApi = 'http://localhost/vendas/'
 
   constructor(public http: HttpClient) {
 
